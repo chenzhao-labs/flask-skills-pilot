f44ebcba63fa45b7ef5ebb1c3e0e08b63363fe9e
chenzhao-labs <486391558@qq.com>
2026-08-15 12:42:31 +0800

    fix: allow views to enable automatic options

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -89,6 +89,19 @@ def test_provide_automatic_options_attr():
     assert sorted(rv.allow) == ["OPTIONS"]
 
 
+def test_provide_automatic_options_attr_enables_options_over_config():
+    app = flask.Flask(__name__)
+    app.config["PROVIDE_AUTOMATIC_OPTIONS"] = False
+
+    def index():
+        return "Hello World!"
+
+    index.provide_automatic_options = True
+    app.route("/")(index)
+    rv = app.test_client().open("/", method="OPTIONS")
+    assert sorted(rv.allow) == ["GET", "HEAD", "OPTIONS"]
+
+
 def test_provide_automatic_options_kwarg(app, client):
     def index():
         return flask.request.method
